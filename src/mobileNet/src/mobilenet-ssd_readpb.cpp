9ea6961ec4e06db2fe8e671f787fac75046cc2dd
lvjianghao <ljao1994@163.com>
2019-04-22 07:29:36 +0800

    update

--- src/mobileNet/src/mobilenet-ssd_readpb.cpp
+++ src/mobileNet/src/mobilenet-ssd_readpb.cpp
@@ -22,6 +22,17 @@
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/core/util/command_line_flags.h"
 
+//opencv
+#include <opencv2/core.hpp>
+#include <opencv/highgui.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv/highgui.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
 using namespace std ;
 using namespace tensorflow;
 using tensorflow::Flag;
@@ -30,10 +41,15 @@ using tensorflow::Status;
 using tensorflow::string;
 using tensorflow::int32;
 
+using namespace cv;
 using namespace std;
 using namespace tensorflow;
 using namespace tensorflow::ops;
 
+
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const float WHRatio = inWidth / (float)inHeight;
 const vector<string > classNames = { "background",
                              "person",
                              "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -43,8 +59,8 @@ const vector<string > classNames = { "background",
                              "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
                              "scissors", "teddy bear", "hair drier", "toothbrush"};
 
-string MNIST_MODEL_PATH  = "/home/haosen/gitPro/catkin_new/data/ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03/frozen_inference_graph.pb";
-
+string MODEL_PATH  = "/home/haosen/gitPro/catkin_new/data/ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03/frozen_inference_graph.pb";
+string Image_path ="/home/haosen/catkin_ws/src/faster_rcnn_tf/data/demo/000456.jpg";
 
 //从文件名中读取数据
 Status ReadTensorFromImageFile(string file_name, const int input_height,
@@ -85,7 +101,7 @@ Status ReadTensorFromImageFile(string file_name, const int input_height,
 int Read_pb()
 {
     GraphDef graph_def;
-    Status status = ReadBinaryProto(Env::Default(),MNIST_MODEL_PATH,&graph_def);
+    Status status = ReadBinaryProto(Env::Default(),MODEL_PATH,&graph_def);
     if(!status.ok())
     {
         cout<<status.ToString()<<endl;
@@ -107,9 +123,7 @@ int Read_pb()
     int input_height = 300;
     int input_width = 300;
     vector<Tensor> inputs;
-    // string image_path(argv[1]);
-    string image_path("/home/haosen/catkin_ws/src/faster_rcnn_tf/data/demo/000542.jpg");
-    if (!ReadTensorFromImageFile(image_path, input_height, input_width,&inputs).ok()) {
+    if (!ReadTensorFromImageFile(Image_path, input_height, input_width,&inputs).ok()) {
         cout<<"Read image file failed"<<endl;
         return -1;
     }
@@ -160,7 +174,88 @@ int Read_pb()
            <<endl<<outputs[1].DebugString()<<endl
           <<endl<<classNames[(tmap3(0)-1)]<<endl
             <<"detection_scores:"<<tmap1<<std::endl
+              <<"detection_boxes:"<<endl<<outputs[2].DebugString()<<endl
            <<"detection_boxes:"<<endl<<outputs[2].tensor<float,3>()<<endl;
+
+    Tensor boxes = Tensor(DT_FLOAT,{100,4});
+    auto boxestemp = boxes.tensor<float,2>();
+    for(int i=0;i<outputs[2].dim_size(1);i++)
+    {
+        for(int j=0;j<outputs[2].dim_size(2);j++)
+        {
+            boxestemp(i,j)=
+            outputs[2].tensor<float,3>()(0,i,j);
+        }
+    }
+    std::cout<<boxes.tensor<float,2>()<<std::endl;
+
+    //show
+    cv::Mat frame = cv::imread(Image_path);
+    cv::Size frame_size = frame.size();
+
+    cv::Size cropSize;
+    if (frame_size.width / (float)frame_size.height > WHRatio)
+    {
+        cropSize = cv::Size(static_cast<int>(frame_size.height * WHRatio),
+            frame_size.height);
+    }
+    else
+    {
+        cropSize =cv::Size(frame_size.width,
+            static_cast<int>(frame_size.width / WHRatio));
+    }
+
+    cv::Rect crop(cv::Point((frame_size.width - cropSize.width) / 2,
+        (frame_size.height - cropSize.height) / 2),
+        cropSize);
+
+
+    frame = frame(crop);
+    float confidenceThreshold = 0.30;
+    for (int i = 0; i < boxes.dim_size(1); i++)
+    {
+        float confidence = boxestemp(i, 2);
+        std::cout<<"confidence:"<<confidence<<endl;
+
+        if (confidence > confidenceThreshold)
+        {
+            size_t objectClass = (size_t)(boxestemp(i, 1));
+
+            int xLeftBottom = static_cast<int>(boxestemp(i, 3) * frame.cols);
+            int yLeftBottom = static_cast<int>(boxestemp(i, 4) * frame.rows);
+            int xRightTop = static_cast<int>(boxestemp(i, 5) * frame.cols);
+            int yRightTop = static_cast<int>(boxestemp(i, 6) * frame.rows);
+            std::cout<<"x,y,x,y :"<<xLeftBottom<<endl
+                    <<yLeftBottom<<endl
+                   <<xRightTop<<endl
+                  <<yRightTop<<endl;
+
+            ostringstream ss;
+            ss << confidence;
+            cv::String conf(ss.str());
+
+            Rect object((int)xLeftBottom, (int)yLeftBottom,
+                (int)(xRightTop - xLeftBottom),
+                (int)(yRightTop - yLeftBottom));
+
+            rectangle(frame, object, Scalar(0, 255, 0),2);
+            rectangle(frame,cv::Point(xLeftBottom,yLeftBottom),Point(xRightTop - xLeftBottom,yRightTop - yLeftBottom),Scalar(255,0,0),1,1,0);
+            String label = String(classNames[objectClass]) + ": " + conf;
+            std::cout<<"className:"<<label<<endl;
+            int baseLine = 0;
+            cv::Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+            rectangle(frame, cv::Rect(cv::Point(xLeftBottom, yLeftBottom - labelSize.height),
+                cv::Size(labelSize.width, labelSize.height + baseLine)),
+                Scalar(0, 255, 0), CV_FILLED);
+            putText(frame, label, Point(xLeftBottom, yLeftBottom),
+                FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
+        }
+    }
+
+    cv::imshow("image", frame);
+    cv::waitKey(0);
+
+
 
 }
 
