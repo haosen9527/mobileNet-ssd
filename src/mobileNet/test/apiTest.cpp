a48ed8f509209b83c04c18b6e27fb8482444cc00
haosen9527 <ljhao1994@163..com>
2019-05-17 09:12:26 +0800

    compair

--- src/mobileNet/test/apiTest.cpp
+++ src/mobileNet/test/apiTest.cpp
@@ -1,7 +1,7 @@
 #include <mobileNet/pbtotensorflow.h>
 
-#define MODEL_PATH  "/home/ywx/mobileNet-ssd/model/frozen_inference_graph.pb"
-#define Image_path "/home/ywx/boxes/micros/src/micros/behavior_management/orient/data/simulation/2.jpg"
+#define MODEL_PATH  "./model/frozen_inference_graph.pb"
+#define Image_path "./result-Img/source.jpg"
 int main()
 {
     pbToTensorflow test(MODEL_PATH);
